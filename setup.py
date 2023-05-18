4de5bc372bba35e1e891c5e734855d87ea3bcbf4
Adam K Dean <adamkdean@googlemail.com>
2023-05-18 21:31:46 +0100

    chore: add setup/reqs

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2023 FlexDB <team@flexdb.co>
+# Use of this source code is governed by the GPL-3.0
+# license that can be found in the LICENSE file.
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
